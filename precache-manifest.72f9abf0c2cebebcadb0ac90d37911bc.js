b1ded271877a58ab88cd50d8f7a5fb0051d91f08
3Dimitra <67105466+3Dimitra@users.noreply.github.com>
2020-07-09 18:30:35 +0300

    Add files via upload

---
+++ precache-manifest.72f9abf0c2cebebcadb0ac90d37911bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed9c8ee8e7ad5c85de234c6e98e5299",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb73bcc46d2da8f6e944",
+    "url": "/static/css/main.bccec5ce.chunk.css"
+  },
+  {
+    "revision": "d6a959dfa300eab8ec85",
+    "url": "/static/js/2.7e4efcea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7e4efcea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb73bcc46d2da8f6e944",
+    "url": "/static/js/main.c7c34ac0.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
